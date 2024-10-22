3c45e6ec39cba65fad2f38a702d460b447ebe7b2
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2024-10-22 19:43:02 +0000

    Co-authored-by: A-A-RON-SHELBY <A-A-RON-SHELBY@users.noreply.github.com> Co-authored-by: amon2009 <amon2009@users.noreply.github.com>

---
+++ HANG_THE_MAN.py
@@ -0,0 +1,65 @@
+#HANG MAN  by Aaron, and Tate, and Martin, and Amon
+import random
+
+x = 0
+
+while x < 27:
+    print(x)
+    x+=1
+#Variables: EVERYONE
+spa_1 = 1
+spa_2 = 2 
+spa_3 = 3
+spa_4 = 4 
+Next_body_part = "Head"
+#Remember to write the input for making a guess
+
+#Function of random 4 letter word, and add or not add a body part depending on the letter chosen: Aaron Shelby
+def func(guess)
+
+def func(wrong)
+    if Next_body_part = "Head":
+
+
+#Print he gallows to hang the man: Tate Morgan 
+Layer_7 = "    ___"
+Layer_6 = "   /   |"
+Layer_5 = "       |"
+Layer_4 = "       |"
+Layer_3 = "       |"
+Layer_2 = "       |"
+Layer_1 = "   ---------"
+print(Layer_7)
+print(Layer_6)
+print(Layer_5)
+print(Layer_4)
+print(Layer_3)
+print(Layer_2)
+print(Layer_1)
+#Check the input: Amon Dipp
+if input("Tell a letter") ==  "n":
+    spa_2= "n"
+elif input(" Tell a letter") == "s":
+    spa_1 = "s"
+elif input("Tell a letter") == "o":
+    spa_3= "o"
+elif input("Tell me letter") == "w":
+    spa_4 = "w"
+
+
+
+
+# guess1 = int(input("guess a letter\n"))
+
+# Update this part when the rest are done (Variable, Function, etc.)
+
+for i in range(8, usr + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print()
+    elif i % 3 == 0:
+        print()
+    elif i % 5 == 0:
+        print()
+    else:
+        print(i)        print(i)
+
